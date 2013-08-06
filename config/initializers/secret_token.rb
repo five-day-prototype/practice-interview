6446fc318df1e1b304f85670570318e1691160a3
Kyle Shipley <kyleashipley@gmail.com>
2013-08-06 17:26:18 -0400

    Way too big initial commit + home page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PracticeInterview::Application.config.secret_key_base = 'cc554f406082d2cb6171695f0d4d88368d3da23b6d11d43230d48a5b839bcb14815333793a716e0479347c3a884056fde2e92cfbb7f3afb595debc9a4bc9adca'
