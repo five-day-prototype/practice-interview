d291621e4d99e8d8dd8dd317950ffd25a105e351
Kyle Shipley <kyleashipley@gmail.com>
2013-08-12 16:43:04 -0400

    Clean up interior form.

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,7 +1,7 @@
 class Interview < ActiveRecord::Base
   belongs_to :user
 
-  validates :job_url, presence: true, format: URI::regexp(%w(http https))
+  validates :job_url, presence: true
   validates :resume_url, presence: true, format: URI::regexp(%w(http https))
   validates :description, presence: true
   validates :time, presence: true
